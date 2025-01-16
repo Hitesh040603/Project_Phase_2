f531b1c6ec09a26f5a379b49720173528abc2d02
unknown <prhitesh1@gmail.com>
2025-01-16 13:12:43 +0530

    Implemented wallet and transactions... to implement tests for the transactions

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -32,6 +32,17 @@ class Blockchain:
         """Serialize blockchain to list of blocks
         """
         return list(map(lambda block: block.to_json(),self.chain))
+    @staticmethod
+    def from_json(chain_json):
+
+        """
+        Deserialize a list into a blockchain instance.
+        Result contains chain list of Block instances
+        """
+        blockchain=Blockchain()
+        blockchain.chain=list(map(lambda block_json: Block.from_json(block_json),chain_json))
+
+        return blockchain
 
     
 
