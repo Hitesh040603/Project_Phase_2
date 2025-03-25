93998940675743b89289b5fd51f6ae1938d9e8e2
unknown <prhitesh1@gmail.com>
2025-03-25 15:50:50 +0530

    added form entry for chips

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -3,25 +3,25 @@ from backend.util.hex_to_binary import hex_to_binary
 from backend.util.crypto_hash import crypto_hash
 from backend.config import MINE_RATE
 
-GENESIS_DATA={
-    'timestamp':1,
+GENESIS_DATA = {
+    'timestamp': 1,
     'last_hash': 'genesis_last_hash',
-    'hash':'genesis_hash',
+    'hash': 'genesis_hash',
     'data': [],
-    'difficulty':3,
-    'nonce':'genesis_nonce'
+    'difficulty': 3,
+    'nonce': 'genesis_nonce',
+    'chip_info': {'chip_id': None, 'chip_make': None, 'current_status': None}
 }
 
 class Block:
-    def __init__(self,timestamp,last_hash,hash,data,difficulty,nonce):
-        self.timestamp=timestamp
-        self.data=data
-        self.last_hash=last_hash
-        self.hash=hash
-        self.nonce=nonce
-        self.difficulty=difficulty
-
-
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce, chip_info=None):
+        self.timestamp = timestamp
+        self.data = data
+        self.last_hash = last_hash
+        self.hash = hash
+        self.nonce = nonce
+        self.difficulty = difficulty
+        self.chip_info = chip_info or {'chip_id': None, 'chip_make': None, 'current_status': None}
 
     def __repr__(self):
         return (f'Block('
@@ -30,37 +30,35 @@ class Block:
                 f'last_hash: {self.last_hash}, '
                 f'data: {self.data}, '
                 f'nonce: {self.nonce}, '
-                f'difficulty: {self.difficulty})')
+                f'difficulty: {self.difficulty}, '
+                f'chip_info: {self.chip_info})')
     
     def __eq__(self, other):
-        return self.__dict__== other.__dict__
+        return self.__dict__ == other.__dict__
     
     def to_json(self):
-        """Serialize block into disctionary
-        """
+        """Serialize block into dictionary"""
         return self.__dict__
 
     @staticmethod
-    def mine_block(last_block,data):
-        """"
-        Mines block based on given last block and data
-        until blockhash meets leading zeroes PoW requirement
+    def mine_block(last_block, data, chip_info=None):
         """
-        timestamp=time.time_ns()
-        last_hash=last_block.hash
-
-        difficulty=last_block.adjust_difficulty(last_block,timestamp)
-        nonce=0
-
-        hash=crypto_hash(timestamp,last_hash,data,difficulty,nonce)
-        while hex_to_binary(hash)[0:difficulty]!= '0'*difficulty:
-            nonce+=1
-            timestamp=time.time_ns()
-            difficulty=last_block.adjust_difficulty(last_block,timestamp)
-
-            hash=crypto_hash(timestamp,last_hash,data,difficulty,nonce)
-
-        return Block(timestamp,last_hash,hash,data,difficulty,nonce)
+        Mines block based on given last block and data
+        until block hash meets leading zeroes PoW requirement
+        """
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        difficulty = last_block.adjust_difficulty(last_block, timestamp)
+        nonce = 0
+        
+        hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce, chip_info)
+        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty:
+            nonce += 1
+            timestamp = time.time_ns()
+            difficulty = last_block.adjust_difficulty(last_block, timestamp)
+            hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce, chip_info)
+        
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce, chip_info)
     
     @staticmethod
     def genesis():
@@ -72,60 +70,49 @@ class Block:
     @staticmethod
     def from_json(block_json):
         """
-        Deserialize a json representation into a block instance
+        Deserialize a JSON representation into a Block instance
         """
         return Block(**block_json)
 
     @staticmethod
-    def adjust_difficulty(last_block,new_timestamp):
+    def adjust_difficulty(last_block, new_timestamp):
         """
         Calculate adjusted difficulty according to MINE_RATE
         Increase if too easy
         Decrease if too difficult
         """
-        if (new_timestamp-last_block.timestamp)<MINE_RATE:
-            return last_block.difficulty+1
-        if last_block.difficulty>1:
-            return last_block.difficulty-1
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+        if last_block.difficulty > 1:
+            return last_block.difficulty - 1
         return 1
     
     @staticmethod
-    def is_valid_block(last_block,block):
+    def is_valid_block(last_block, block):
         """
         Validate block by enforcing rules:
-        1. Block must have crct last_hash reference
+        1. Block must have correct last_hash reference
         2. Block must satisfy PoW requirement
         3. Difficulty must only be adjusted by 1
-        4. Block hash must match 
+        4. Block hash must match
         """
-        if block.last_hash!=last_block.hash:
+        if block.last_hash != last_block.hash:
             raise Exception('The block last_hash must be correct')
-
-        if hex_to_binary(block.hash)[0:block.difficulty]!='0'*block.difficulty:
+        
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
             raise Exception('PoW requirement was not met')
         
-        if abs(last_block.difficulty-block.difficulty)>1:
+        if abs(last_block.difficulty - block.difficulty) > 1:
             raise Exception('Difficulty difference is more than 1')
         
-        reconstructed_hash=crypto_hash(block.timestamp,
-                                       block.last_hash,
-                                       block.data,
-                                       block.nonce,
-                                       block.difficulty)
-        if block.hash!=reconstructed_hash:
+        reconstructed_hash = crypto_hash(block.timestamp, block.last_hash, block.data, block.nonce, block.difficulty, block.chip_info)
+        if block.hash != reconstructed_hash:
             raise Exception('Block hash is not correct')
 
-
-    
-
 def main():
-    genesis_block=Block.genesis()
-    bad_block=Block.mine_block(genesis_block,'foo')
-    
-    print(bad_block)
+    genesis_block = Block.genesis()
+    new_block = Block.mine_block(genesis_block, 'foo', {'chip_id': '12345', 'chip_make': 'ChipCorp', 'current_status': 'active'})
+    print(new_block)
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -2,7 +2,7 @@ import uuid
 import time
 
 from backend.wallet.wallet import Wallet
-from backend.config import MINING_REWARD,MINING_REWARD_INPUT
+from backend.config import MINING_REWARD, MINING_REWARD_INPUT
 
 
 class Transaction:
@@ -15,54 +15,62 @@ class Transaction:
                  amount=None,
                  id=None,
                  output=None,
-                 input=None
-                 ):
-        self.id=id or str(uuid.uuid4())[0:8]
-        self.output=output or self.create_output(sender_wallet,recipient,amount)
-        self.input=input or self.create_input(sender_wallet,self.output)
+                 input=None,
+                 chip_info=None):
+        self.id = id or str(uuid.uuid4())[0:8]
+        self.output = output or self.create_output(sender_wallet, recipient, amount)
+        self.input = input or self.create_input(sender_wallet, self.output)
+        self.chip_info = chip_info or {'chip_id': None, 'chip_make': None, 'current_status': None}
 
-
-    def create_output(self,sender_wallet,recipient,amount):
+    def create_output(self, sender_wallet, recipient, amount):
         """
-        structures output data for transaction
+        Structures output data for transaction
         """
-        if amount>sender_wallet.balance:
+        if amount > sender_wallet.balance:
             raise Exception('Amount exceeds balance')
-        output={}
-        output[recipient]=amount
-        output[sender_wallet.address]=sender_wallet.balance-amount
+        output = {}
+        output[recipient] = amount
+        output[sender_wallet.address] = sender_wallet.balance - amount
 
         return output
     
-    def create_input(self,sender_wallet,output):
+    def create_input(self, sender_wallet, output):
         """
         Structure input data for transaction
         Sign transaction and include sender's public key and address
         """
-
-        return{
+        return {
             'timestamp': time.time_ns(),
-            'amount':sender_wallet.balance,
-            'address':sender_wallet.address,
-            'public_key':sender_wallet.public_key,
-            'signature':sender_wallet.sign(output)
+            'amount': sender_wallet.balance,
+            'address': sender_wallet.address,
+            'public_key': sender_wallet.public_key,
+            'signature': sender_wallet.sign(output)
         }
     
-    def update(self,sender_wallet,recipient,amount):
+    def update(self, sender_wallet, recipient, amount):
         """
         Update transaction with existing or new recipient
         """
-        if amount>self.output[sender_wallet.address]:
+        if amount > self.output[sender_wallet.address]:
             raise Exception('Amount exceeds balance')
         
-        if  recipient in self.output:
-            self.output[recipient]+=amount
+        if recipient in self.output:
+            self.output[recipient] += amount
         else:
-            self.output[recipient]=amount
+            self.output[recipient] = amount
 
-        self.output[sender_wallet.address]-=amount
+        self.output[sender_wallet.address] -= amount
+        self.input = self.create_input(sender_wallet, self.output)
 
-        self.input=self.create_input(sender_wallet,self.output)
+    def update_chip_info(self, chip_id, chip_make, current_status):
+        """
+        Update chip information in the transaction
+        """
+        self.chip_info = {
+            'chip_id': chip_id,
+            'chip_make': chip_make,
+            'current_status': current_status
+        }
 
     def to_json(self):
         """
@@ -73,51 +81,46 @@ class Transaction:
     @staticmethod
     def from_json(transaction_json):
         """
-        Deserialize transaction's json representation back into Transaction instance
+        Deserialize transaction's JSON representation back into Transaction instance
         """
         return Transaction(**transaction_json)
-
-
 
     @staticmethod
     def is_valid_transaction(transaction):
         """
         Validates transactions and raises exception if not
         """
-        if transaction.input==MINING_REWARD_INPUT:
-            if list(transaction.output.values())!=[MINING_REWARD]:
+        if transaction.input == MINING_REWARD_INPUT:
+            if list(transaction.output.values()) != [MINING_REWARD]:
                 raise Exception('Invalid mining reward')
             return
 
-
-        output_total=sum(transaction.output.values())
-        if transaction.input['amount']!=output_total:
+        output_total = sum(transaction.output.values())
+        if transaction.input['amount'] != output_total:
             raise Exception('Invalid transaction output values')
         
-        if not Wallet.verify(transaction.input['public_key'],transaction.output,transaction.input['signature']):
+        if not Wallet.verify(transaction.input['public_key'], transaction.output, transaction.input['signature']):
             raise Exception('Invalid signature')
         
     @staticmethod
     def reward_transaction(miner_wallet):
         """
-        Generated reward transation for miner
+        Generate reward transaction for miner
         """
-        output={}
-        output[miner_wallet.address]=MINING_REWARD
+        output = {}
+        output[miner_wallet.address] = MINING_REWARD
 
-        return Transaction(input=MINING_REWARD_INPUT,output=output)
+        return Transaction(input=MINING_REWARD_INPUT, output=output)
         
 
-
-
-        
 def main():
-    transaction=Transaction(Wallet(),'recipient',15)
+    transaction = Transaction(Wallet(), 'recipient', 15)
+    transaction.update_chip_info('12345', 'ChipCorp', 'active')
     print(f'transaction.__dict__: {transaction.__dict__}')
-    transaction_json=transaction.to_json()
-    restored_transaction=Transaction.from_json(transaction_json)
+    transaction_json = transaction.to_json()
+    restored_transaction = Transaction.from_json(transaction_json)
 
     print(f'restored_transaction.__dict__: {restored_transaction.__dict__}')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
